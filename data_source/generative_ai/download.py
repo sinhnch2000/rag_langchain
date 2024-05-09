83f8a4cca82dbd8397433c23ca22c68f6c726e9d
This PC <sinhnch2001@gmail.com>
2024-05-09 19:47:48 +0700

    update files

---
+++ data_source/generative_ai/download.py
@@ -0,0 +1,28 @@
+import os
+import wget
+
+file_links = [
+    {
+        "title": "Attention Is All You Need",
+        "url": "https://arxiv.org/pdf/1706.03762"
+    } ,
+    {
+        "title": "BERT- Pre-training of Deep Bidirectional Transformers for Language Understanding",
+        "url": "https://arxiv.org/pdf/1810.04805"
+    } ,
+    {
+        "title": "Chain-of-Thought Prompting Elicits Reasoning in Large Language Models",
+        "url": "https://arxiv.org/pdf/2201.11903"
+    } ,
+    {
+        "title": "Instruction Tuning for Large Language Models- A Survey",
+        "url": " https://arxiv.org/pdf/2308.10792"
+    }
+]
+
+def is_exist(file_link):
+    return os.path.exists(f"./{file_link['title']}.pdf")
+
+for file_link in file_links:
+    if not is_exist(file_link):
+        wget.download(file_link["url"], out=f"./{file_link['title']}.pdf")
